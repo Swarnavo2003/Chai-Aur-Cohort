eda59ab8c8ea0dc9a227bc9a5dd43f9869528670
Swarnavo2003 <swarnavo2020@gmail.com>
2025-02-08 22:54:02 +0530

    Conditional Challenges Added

---
+++ Week-4/Assignments/Condiionals/Challenge-9.js
@@ -0,0 +1,26 @@
+// You just need to implement the convertTemperature function
+function convertTemperature(value, scale) {
+  // Convert temperature based on the scale ("C" to "F" or "F" to "C")
+  switch (scale) {
+    case "C":
+      return `${(value * 9) / 5 + 32}°F`;
+    case "F":
+      return `${(value - 32) * (5 / 9)}°C`;
+    default:
+      return "Invalid Scale";
+  }
+}
+
+// Please don't remove the code below
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.on("line", (input) => {
+  const { value, scale } = JSON.parse(input);
+  const result = convertTemperature(value, scale);
+  process.stdout.write(JSON.stringify(result));
+});
